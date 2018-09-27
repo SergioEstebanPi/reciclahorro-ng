0ca795bb86e2227a0bcd60463398480ebb2b47c7
mac <mac@macs-MacBook-Pro.local>
2018-09-27 07:25:11 -0500

    Configuro para desplige con express y heroku

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname + '/dist/reciclahorro-ng'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/reciclahorro-ng/index.html'));
+res.sendFile(path.join(__dirname+'/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
